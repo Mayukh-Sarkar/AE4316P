00d41e8dfa76dd7a873a4caf86b8529e5e47dc23
Mayukh <mayukhsarkar66@gmail.com>
2021-03-25 20:56:35 +0100

    press F for learning curve

---
+++ f.m
@@ -0,0 +1,3 @@
+function y = f(constant,x)
+y = constant(3) +(1-constant(2)).^x*(constant(1)-constant(3));
+end
--- testfile.m
+++
@@ -1,7 +0,0 @@
-%rng default % for reproducibility
-tdata = 0:0.1:10;
-ydata = 40*exp(-0.5*tdata) + randn(size(tdata));
-
-fun = @(x)sseval(x,tdata,ydata);
-x0 = rand(2,1);
-bestx = fminsearch(fun,x0)
