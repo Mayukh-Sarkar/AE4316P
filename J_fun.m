6d01a83c7bc10337f199754028ed5268f91bfc35
SybrenBootsma <47575390+SybrenBootsma@users.noreply.github.com>
2021-04-27 23:09:08 +0200

    Bode plots

--- J_fun.m
+++ J_fun.m
@@ -2,17 +2,23 @@
 
 load('ae4316P_2021_data_group1.mat')
 
-N_runs = 5 ;
-N_participants = 8 ;
+% N_runs = 5 ;
+N_participants = 1 ;
 N_parameters = 5 ;
-% N_runs = 60 ;
+N_runs = 60 ;
 human_par_M_Kp = zeros(N_runs,N_participants) ;
 human_par_M_TL = zeros(N_runs,N_participants) ;
 human_par_M_tau_p = zeros(N_runs,N_participants) ;
 human_par_M_zeta_nm = zeros(N_runs,N_participants) ;
 human_par_M_omega_nm = zeros(N_runs,N_participants) ;
 omega = w ;
-options = struct('MaxFunEvals', 2000,'MaxIter', 1000);
+omega_test = logspace(-1, 1.5, 200) ;
+options = struct('MaxFunEvals', 5000,'MaxIter', 5000);
+
+data_mag1 = zeros(N_runs, length(omega)) ;
+data_phase1 = zeros(N_runs, length(omega)) ;
+data_mag2 = zeros(N_runs, length(omega_test)) ;
+data_phase2 = zeros(N_runs, length(omega_test)) ;
 
 for i = 1 : N_runs
     phase_M_1 = groupM_data_subj1.phase_Hp(:,i) ;
@@ -47,6 +53,8 @@ for i = 1 : N_runs
             H_pe(k) = Mag(k) * (cos(Phase(k)*pi/180) + 1j * sin(Phase(k)*pi/180)) ;
             g = @(x) (abs(H_pe(k) - x(1)*(1 + x(2)*(1j*w(k))) * exp(-1j*w(k)*x(3)) * (x(5)^2/(x(5)^2 + 2*x(4)*x(5)*1j*w(k) + (1j*w(k))^2))))^2;
             f = @(x) f(x) + g(x) ;
+            data_mag1(i,k) = Mag(k) ;
+            data_phase1(i,k) = Phase(k) ;
         end
 
         x0 = [3, 1, 0.35, 0.5, 15] ;
@@ -59,34 +67,251 @@ for i = 1 : N_runs
         human_par_M_zeta_nm(i,m) = x(4) ;
         human_par_M_omega_nm(i,m) = x(5) ;
 
-
-        omega_test = logspace(-1, 1.5, 200) ;
         phase_out = zeros(length(omega_test),1) ;
         mag_out = zeros(length(omega_test),1) ;
-
+        
+        get_i = 100000 ;
+        get_ii = 100000 ;
+        get_iii = 100000 ;
+        count = 0 ;
         for l = 1:length(omega_test)
             Hpe_model = x(1)*(1 + x(2)*(1j*omega_test(l))) * exp(-1j*omega_test(l)*x(3)) * (x(5)^2/(x(5)^2 + 2*x(4)*x(5)*1j*omega_test(l) + (1j*omega_test(l))^2));
             mag_out(l) = abs(Hpe_model) ;
             phase_out(l) = angle(Hpe_model)*180/pi ;
+            if (phase_out(l) < -160) && (count == 0)
+                get_i = l ;
+                count = 1 ;
+            end
+            if (l >= get_i) && (phase_out(l) > -160)
+                phase_out(l) = phase_out(l) - 360 ;
+            end
+            if (phase_out(l) < -480) && (count == 1)
+                get_ii = l ;
+                count = 2 ;
+            end
+            if (l >= get_ii) && (phase_out(l) > -480)
+                phase_out(l) = phase_out(l) - 360 ;
+            end
+%             if (phase_out(l) < -880) && (count == 2)
+%                 get_iii = l ;
+%                 count = 3 ;
+%             end
+%             if (l >= get_iii) && (phase_out(l) > -880)
+%                 phase_out(l) = phase_out(l) - 360 ;
+%             end
+            data_mag2(i,l) = mag_out(l) ;
+            data_phase2(i,l) = phase_out(l) ;
         end
         
-        title = "Bode plot for run #" + num2str(i) + " from participant #" + num2str(m) ;
-        figure(7+i)
-        subplot(2,1,1)
-        loglog(omega, mag_M(:,m), 'o')
-        hold on
-        loglog(omega_test, mag_out)
-        hold off
-        axis(10.^[-1 1.5 -1 2])
-        legend('Experiment','Parameter estimation','Location','southwest')
-
-        subplot(2,1,2)
-        semilogx(omega, phase_M(:,m), 'o')
-        hold on
-        semilogx(omega_test, phase_out)
-        hold off
-        axis([10.^-1 10.^1.5 -360 180])
-        legend('Experiment','Parameter estimation','Location','southwest')
-        sgtitle(title)
+%         title = "Bode plot for run #" + num2str(i) + " from participant #" + num2str(m) ;
+% %         figure(N_participants*(i-1)+m)
+%         figure(i)
+%         subplot(2,1,1)
+%         loglog(omega, mag_M(:,m), 'o')
+%         hold on
+%         loglog(omega_test, mag_out)
+%         hold off
+%         axis(10.^[-1 1.5 -1 2])
+%         legend('Experiment','Parameter estimation','Location','southwest')
+% 
+%         subplot(2,1,2)
+%         semilogx(omega, phase_M(:,m), 'o')
+%         hold on
+%         semilogx(omega_test, phase_out)
+%         hold off
+%         axis([10.^-1 10.^1.5 -360 180])
+%         legend('Experiment','Parameter estimation','Location','southwest')
+%         sgtitle(title)
     end
 end
+
+
+title = "Bode plot for run 1 and 60 from participant 1 in Moving simulator" ;
+figure(1)
+set(gcf, 'Position', [100 100 700 650])
+subplot(2,1,1)
+loglog(omega, data_mag1(1,:),'ok')
+hold on
+loglog(omega, data_mag1(60,:),'*k')
+hold on
+loglog(omega_test, data_mag2(1,:),'-k')
+hold on
+loglog(omega_test, data_mag2(60,:),'--k')
+hold off
+axis(10.^[-1 1.5 -1 2])
+xlabel("\omega [rad/s]")
+ylabel("|H_{p} (j \omega)| [-]")
+% legend('Test data run 1','Test data run 60','Model run 1','Model run 60','Location','southwest')
+
+subplot(2,1,2)
+semilogx(omega, data_phase1(1,:),'ok')
+hold on
+semilogx(omega, data_phase1(60,:),'*k')
+hold on
+loglog(omega_test, data_phase2(1,:),'-k')
+hold on
+loglog(omega_test, data_phase2(60,:),'--k')
+hold off
+axis([10.^-1 10.^1.5 -360 180])
+xlabel("\omega [rad/s]")
+ylabel("\angle H_{p} (j \omega) [deg]")
+legend('Test data run 1','Test data run 60','Model run 1','Model run 60','Location','southwest')
+sgtitle(title)
+
+%% Non moving
+
+% N_runs = 5 ;
+N_participants = 1 ;
+N_parameters = 5 ;
+N_runs = 60 ;
+human_par_NM_Kp = zeros(N_runs,N_participants) ;
+human_par_NM_TL = zeros(N_runs,N_participants) ;
+human_par_NM_tau_p = zeros(N_runs,N_participants) ;
+human_par_NM_zeta_nm = zeros(N_runs,N_participants) ;
+human_par_NM_omega_nm = zeros(N_runs,N_participants) ;
+omega = w ;
+omega_test = logspace(-1, 1.5, 200) ;
+options = struct('MaxFunEvals', 5000,'MaxIter', 2000);
+
+data_mag1_NM = zeros(N_runs, length(omega)) ;
+data_phase1_NM = zeros(N_runs, length(omega)) ;
+data_mag2_NM = zeros(N_runs, length(omega_test)) ;
+data_phase2_NM = zeros(N_runs, length(omega_test)) ;
+
+for i = 1 : N_runs
+    phase_NM_1 = groupNM_data_subj1.phase_Hp(:,i) ;
+    phase_NM_2 = groupNM_data_subj2.phase_Hp(:,i) ;
+    phase_NM_3 = groupNM_data_subj3.phase_Hp(:,i) ;
+    phase_NM_4 = groupNM_data_subj4.phase_Hp(:,i) ;
+    phase_NM_5 = groupNM_data_subj5.phase_Hp(:,i) ;
+    phase_NM_6 = groupNM_data_subj6.phase_Hp(:,i) ;
+    phase_NM_7 = groupNM_data_subj7.phase_Hp(:,i) ;
+    phase_NM_8 = groupNM_data_subj8.phase_Hp(:,i) ;
+    phase_NM = [phase_NM_1 phase_NM_2 phase_NM_3 phase_NM_4 phase_NM_5 phase_NM_6 phase_NM_7 phase_NM_8];
+    phase_mean_NM = mean(phase_NM, 2) ;
+
+    mag_NM_1 = groupNM_data_subj1.mag_Hp(:,i) ;
+    mag_NM_2 = groupNM_data_subj2.mag_Hp(:,i) ;
+    mag_NM_3 = groupNM_data_subj3.mag_Hp(:,i) ;
+    mag_NM_4 = groupNM_data_subj4.mag_Hp(:,i) ;
+    mag_NM_5 = groupNM_data_subj5.mag_Hp(:,i) ;
+    mag_NM_6 = groupNM_data_subj6.mag_Hp(:,i) ;
+    mag_NM_7 = groupNM_data_subj7.mag_Hp(:,i) ;
+    mag_NM_8 = groupNM_data_subj8.mag_Hp(:,i) ;
+    mag_NM = [mag_NM_1 mag_NM_2 mag_NM_3 mag_NM_4 mag_NM_5 mag_NM_6 mag_NM_7 mag_NM_8];
+    mag_mean_NM = mean(mag_NM, 2) ;
+
+    for m = 1 : N_participants
+        H_pe = zeros(length(w),1) ; 
+        f = @(x) 0 ;
+
+        Phase = phase_NM(:,m);
+        Mag = mag_NM(:,m);
+        for k = 1 : length(w)
+            H_pe(k) = Mag(k) * (cos(Phase(k)*pi/180) + 1j * sin(Phase(k)*pi/180)) ;
+            g = @(x) (abs(H_pe(k) - x(1)*(1 + x(2)*(1j*w(k))) * exp(-1j*w(k)*x(3)) * (x(5)^2/(x(5)^2 + 2*x(4)*x(5)*1j*w(k) + (1j*w(k))^2))))^2;
+            f = @(x) f(x) + g(x) ;
+            data_mag1_NM(i,k) = Mag(k) ;
+            data_phase1_NM(i,k) = Phase(k) ;
+        end
+
+        x0 = [3, 1, 0.35, 0.5, 15] ;
+        % [x,fval,exitflag,output] = fminsearch(f, x0, options);
+        x = fminsearch(f,x0,options);
+
+        human_par_NM_Kp(i,m) = x(1) ;
+        human_par_NM_TL(i,m) = x(2) ;
+        human_par_NM_tau_p(i,m) = x(3) ;
+        human_par_NM_zeta_nm(i,m) = x(4) ;
+        human_par_NM_omega_nm(i,m) = x(5) ;
+
+        phase_out = zeros(length(omega_test),1) ;
+        mag_out = zeros(length(omega_test),1) ;
+        
+        get_i = 100000 ;
+        get_ii = 100000 ;
+        get_iii = 100000 ;
+        count = 0 ;
+        for l = 1:length(omega_test)
+            Hpe_model = x(1)*(1 + x(2)*(1j*omega_test(l))) * exp(-1j*omega_test(l)*x(3)) * (x(5)^2/(x(5)^2 + 2*x(4)*x(5)*1j*omega_test(l) + (1j*omega_test(l))^2));
+            mag_out(l) = abs(Hpe_model) ;
+            phase_out(l) = angle(Hpe_model)*180/pi ;
+            if (phase_out(l) < -160) && (count == 0)
+                get_i = l ;
+                count = 1 ;
+            end
+            if (l >= get_i) && (phase_out(l) > -160)
+                phase_out(l) = phase_out(l) - 360 ;
+            end
+            if (phase_out(l) < -480) && (count == 1)
+                get_ii = l ;
+                count = 2 ;
+            end
+            if (l >= get_ii) && (phase_out(l) > -480)
+                phase_out(l) = phase_out(l) - 360 ;
+            end
+%             if (phase_out(l) < -880) && (count == 2)
+%                 get_iii = l ;
+%                 count = 3 ;
+%             end
+%             if (l >= get_iii) && (phase_out(l) > -880)
+%                 phase_out(l) = phase_out(l) - 360 ;
+%             end
+            data_mag2_NM(i,l) = mag_out(l) ;
+            data_phase2_NM(i,l) = phase_out(l) ;
+        end
+        
+%         title = "Bode plot for run #" + num2str(i) + " from participant #" + num2str(m) ;
+% %         figure(N_participants*(i-1)+m)
+%         figure(i)
+%         subplot(2,1,1)
+%         loglog(omega, mag_M(:,m), 'o')
+%         hold on
+%         loglog(omega_test, mag_out)
+%         hold off
+%         axis(10.^[-1 1.5 -1 2])
+%         legend('Experiment','Parameter estimation','Location','southwest')
+% 
+%         subplot(2,1,2)
+%         semilogx(omega, phase_M(:,m), 'o')
+%         hold on
+%         semilogx(omega_test, phase_out)
+%         hold off
+%         axis([10.^-1 10.^1.5 -360 180])
+%         legend('Experiment','Parameter estimation','Location','southwest')
+%         sgtitle(title)
+    end
+end
+
+
+title = "Bode plot for run 1 and 60 from participant 1 in Non-Moving simulator" ;
+figure(2)
+set(gcf, 'Position', [100 100 700 650])
+subplot(2,1,1)
+loglog(omega, data_mag1_NM(1,:),'ok')
+hold on
+loglog(omega, data_mag1_NM(60,:),'*k')
+hold on
+loglog(omega_test, data_mag2_NM(1,:),'-k')
+hold on
+loglog(omega_test, data_mag2_NM(60,:),'--k')
+hold off
+axis(10.^[-1 1.5 -1 2])
+xlabel("\omega [rad/s]")
+ylabel("|H_{p} (j \omega)| [-]")
+% legend('Test data run 1','Test data run 60','Model run 1','Model run 60','Location','southwest')
+
+subplot(2,1,2)
+semilogx(omega, data_phase1_NM(1,:),'ok')
+hold on
+semilogx(omega, data_phase1_NM(60,:),'*k')
+hold on
+loglog(omega_test, data_phase2_NM(1,:),'-k')
+hold on
+loglog(omega_test, data_phase2_NM(60,:),'--k')
+hold off
+axis([10.^-1 10.^1.5 -360 180])
+xlabel("\omega [rad/s]")
+ylabel("\angle H_{p} (j \omega) [deg]")
+legend('Test data run 1','Test data run 60','Model run 1','Model run 60','Location','southwest')
+sgtitle(title)
